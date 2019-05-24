5b9fe297cb91aa8f5240ab195e992e3eaa32239c
Noah <savedream75@gmail.com>
2019-05-24 15:31:32 +0900

    #7 Blockchain Creating a Block
    블록체인 데이터를 선언합니다.

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,28 @@
-interface Human {
-  name: string;
-  age: number;
-  gender: string;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
+  }
 }
 
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${
-    person.gender
-  }!`;
-};
-const person = {
-  name: "Minhyeong",
-  age: 23,
-  gender: "male"
-};
-console.log(sayHi(person));
+const genesisBlock: Block = new Block(0, "DA3^D=+ASD*&*", "", "blockData", 210123123);
+
+let blockchain: [Block] = [genesisBlock];
+
+console.log(blockchain);
 
 export {};
