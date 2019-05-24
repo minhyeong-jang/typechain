3446fa973a49e9a5db95293aeb334e9dff33a9d1
Noah <savedream75@gmail.com>
2019-05-24 14:50:46 +0900

    #5 Interfaces on Typescript
    Interface 선언을 학습합니다.

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}!`;
+interface Human {
+  name: string;
+  age: number;
+  gender: string;
+}
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.name}, you are ${person.age}, you are a ${
+    person.gender
+  }!`;
 };
+const person = {
+  name: "Minhyeong",
+  age: 23,
+  gender: "male"
+};
+console.log(sayHi(person));
 
- console.log(sayHi("Minhyeong", 23, "male"));
-
- export {};
+export {};
